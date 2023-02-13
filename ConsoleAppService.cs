292215c794b5c7d3e5155f5ab98af7226b87d574
Aleksas Janulevičius <aseksas@gmail.com>
2023-02-13 11:07:02 +0200

    All repos

---
+++ ConsoleAppService.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess.Data;
+using DataAccess.Models;
+using Microsoft.Extensions.Configuration;
+using TaxiDispacher.Helpers;
+
+namespace ConsoleUI;
+
+public class ConsoleAppService : IConsoleAppService
+{
+    private readonly IUserRepository _userRepository;
+    private string? _securitySalt = null;
+    public ConsoleAppService(IUserRepository userRepository, IConfiguration config)
+    {
+        _userRepository = userRepository;
+        _securitySalt = config["Salt"];
+    }
+    public void Run()
+    {
+        Console.Write("Let's create new ADMIN User:\n\n");
+
+        Console.Write("Username: ");
+        var username = Console.ReadLine();
+        Console.Write("Password: ");
+        var password = Console.ReadLine();
+        try
+        {
+            _userRepository.Create(username, Crypt.Encrypt(password, _securitySalt), UsersModel.ROLE_ADMIN);
+
+            Console.WriteLine(_userRepository.GetAll().Result.Count());
+            Console.WriteLine("\n\nDone, try to login");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.WriteLine("\n\n");
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.Data;
+using DataAccess.DbAccess;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ConsoleUI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder();
+        BuildConfig(builder);
+
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) => {
+                services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+                services.AddSingleton<IUserRepository, UserRepository>();
+                services.AddTransient<IConsoleAppService, ConsoleAppService>();
+            })
+            .Build();
+
+        var svc = ActivatorUtilities.CreateInstance<ConsoleAppService>(host.Services);
+        svc.Run();
+        
+    }
+
+    static void BuildConfig(IConfigurationBuilder builder)
+    {
+        builder.SetBasePath(Directory.GetCurrentDirectory());
+        builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        builder.AddEnvironmentVariables();
+    }
+
+}
